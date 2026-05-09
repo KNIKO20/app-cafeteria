341f7a7f087605f0926071cc4f1227fec28e0fae
Enngel <engelnieves000@gmail.com>
2026-05-09 16:14:44 +0200

    Intento de arreglo para que funcione el railway

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok", "app": "cafeteria-api"})
 
 urlpatterns = [
-#    path('admin/', admin.site.urls),
+    path('', health_check),                          # evita el "Not Found" en /
+    path('admin/', admin.site.urls),                 # DESCOMENTADO 
     path('api/', include('adapters.api.urls')),
 ]
